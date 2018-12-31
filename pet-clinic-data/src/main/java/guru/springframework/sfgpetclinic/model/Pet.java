92d7de9d57d86762f9e0f1466b4e4581dcb4b8c9
Szymon Jaranowski <szymon.jaranowski@gmail.com>
2018-12-31 02:46:58 +0100

    Add Ability to Add or Update a Visit. Closes #57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -16,13 +16,17 @@ import java.util.Set;
 public class Pet extends BaseEntity {
 
     @Builder
-    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate) {
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
         super(id);
 
         this.name = name;
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
     }
 
     @Column(name = "name")
@@ -40,5 +44,5 @@ public class Pet extends BaseEntity {
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
-    private final Set<Visit> visits = new HashSet<>();
+    private Set<Visit> visits = new HashSet<>();
 }
